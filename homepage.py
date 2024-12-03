293e7d773686eeff9374116704249c2ec1e801e2
Cupressa <jonahcl1583@gmail.com>
2024-12-03 09:49:18 -0600

    final updates, improved homepage, modified access-level views

--- homepage.py
+++ homepage.py
@@ -10,7 +10,14 @@ def main():
         page_icon = "https://upload.wikimedia.org/wikipedia/en/thumb/6/6f/Creighton_Bluejays_logo.svg/640px-Creighton_Bluejays_logo.svg.png",
     )
     st.title("CU Pep Band Database - Homepage")
-    
+    col1, col2 = st.columns(2)
+    with col1:
+        st.image("resources\pep_home_1.jpg")
+        st.image("resources\pep_home_2.jpg")
+    with col2:
+        st.image("resources\pep_home_3.jpg")
+        st.image("resources\pep_home_4.jpg")
+    st.write("The Creighton University Pep Band is a student led organization within the Athletic Department. We pride ourselves on being the biggest student-fans around, who just happen to have musical ability. The band plays at all Women's Volleyball, Women's Basketball, and Men's Basketball games, as well as special Athletic Department events. Thus, band members are guaranteed seating at all volleyball and basketball games, as well as a free meal before each game. The band also has the opportunity to support both the men's and women's basketball teams in their postseason tournaments! Be sure to show up to one of our practices to learn more!")
     st.sidebar.success("Select a page")
 
 if __name__=="__main__":
--- pages/view_user_requests.py
+++ pages/view_user_requests.py
@@ -1,27 +1,29 @@
 import streamlit as st
 from sqlalchemy.sql import text
-
-if st.session_state['authentication_status']:
-    if st.session_state["roles"][0] in ["EXECUTIVE", "ADMINISTRATOR"]:
-        conn = st.connection(
-            "sql",
-            dialect="mysql",
-            host="localhost",
-            database = "csc548band",
-            username = "pep_user",
-            password = "userProfile",
-        )
-        requestDF = conn.query("SELECT r.requestId AS requestId, netId, request_timestamp, complete, section, song_title FROM requests r LEFT JOIN folder_requests f ON r.requestId = f.requestId LEFT JOIN song_requests s ON s.requestId = r.requestId WHERE complete = 0;", ttl=0)
-        st.title("View folder and song requests")
-        st.write(requestDF)
-        st.write("Mark request as complete")
-        completeId = st.selectbox(
-            "Select completed request ID", [f"{row.requestId}" for row in requestDF.itertuples()]
-        )
-        if(st.button("Mark as complete")):
-            with conn.session as session:
-                session.execute(text(f"UPDATE requests SET complete=1 WHERE requestId={completeId};"))
-                session.commit()
+try:
+    if st.session_state['authentication_status']:
+        if st.session_state["roles"][0] in ["EXECUTIVE", "ADMINISTRATOR"]:
+            conn = st.connection(
+                "sql",
+                dialect="mysql",
+                host="localhost",
+                database = "csc548band",
+                username = "pep_user",
+                password = "userProfile",
+            )
+            requestDF = conn.query("SELECT r.requestId AS requestId, netId, request_timestamp, complete, section, song_title FROM requests r LEFT JOIN folder_requests f ON r.requestId = f.requestId LEFT JOIN song_requests s ON s.requestId = r.requestId WHERE complete = 0;", ttl=0)
+            st.title("View folder and song requests")
+            st.write(requestDF)
+            st.write("Mark request as complete")
+            completeId = st.selectbox(
+                "Select completed request ID", [f"{row.requestId}" for row in requestDF.itertuples()]
+            )
+            if(st.button("Mark as complete")):
+                with conn.session as session:
+                    session.execute(text(f"UPDATE requests SET complete=1 WHERE requestId={completeId};"))
+                    session.commit()
 
     else:
         st.write("Access denied, please log in or register an account")
+except:
+    st.write("Access denied, please log in or register an account")
